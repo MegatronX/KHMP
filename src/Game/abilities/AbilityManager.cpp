87ad8841f369340b368891475a5281c49170ac37
MegatronX <super24bitsound@hotmail.com>
2012-04-12 14:45:44 -0300

    Update src/Game/abilities/AbilityManager.cpp

--- src/Game/abilities/AbilityManager.cpp
+++ src/Game/abilities/AbilityManager.cpp
@@ -4,6 +4,8 @@ namespace Game
 {
 	namespace Abilities
 	{
+		boost::unordered_map<std::string, boost::unordered_map<std::string, int> > Ability::APCostOverrides; // = boost::unordered_map<std::string, boost::unordered_map<std::string, int> >();
+		boost::unordered_map<std::string, boost::unordered_map<std::string, int> > Ability::RequiredXPOverrides;
 		std::vector<Ability*> EmptyAbilSet;
 		AbilityManager::AbilityManager()
 		{
@@ -23,7 +25,7 @@ namespace Game
 			return EmptyAbilSet;
 		}
 
-		std::vector<Ability*> AbilityManager::GetAbilitiesWithComponent(const std::string* compName, bool SortByPriority) const
+		std::vector<Ability*> AbilityManager::GetAbilitiesWithComponent(const std::string& compName, bool SortByPriority) const
 		{
 			return EmptyAbilSet;
 		}
@@ -35,6 +37,57 @@ namespace Game
 		{
 			return EmptyAbilSet;
 		}
+		
+		bool AbilityManager::AddAbility(const std::string& AbilityName, int instances)
+		{
+			if (abilLib != nullptr)
+			{
+				auto abil = abilLib->GetAbility(AbilityName);
+				if (abil != nullptr)
+				{
+					return AddAbility(abil->Clone(), instances);
+				}
+	#ifdef DEBUG
+				else
+				{
+					std::cerr << "Ability " << AbilityName << " could not be found in database." << std::end;
+				}
+	#endif
+			}
+	#ifdef DEBUG
+			else
+			{
+				std::cerr << "Ability Library is not set!" << std::endl;
+			}
+			return false;
+	#endif
+		}
+		bool AbilityManager::AddAbility(const Ability* ability, int instances)
+		{
+			if (ability != null)
+			{
+				return AddAbility(ability->Clone(), instances);
+			}
+			return false;
+		}
+		bool AbilityManager::AddAbility(abil_ptr ability, int instances)
+		{
+			bool Added = false;
+			
+			
+			return Added;
+		}
+			
+		bool AbilityManager::RemoveAbility(const std::string, int instances)
+		{
+			
+		}
+		
+		void SetAbilityLibrary(AbilityLibrary* lib)
+		{
+			abilLib = lib;
+		}
+		
 		void AbilityManager::DistributeXP(const int XP)
 		{
 			if (AllowXP)
