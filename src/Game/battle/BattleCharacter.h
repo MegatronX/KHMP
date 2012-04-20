e8696541bb868be3f38ea799334a1ce40e74d6bf
MegatronX <super24bitsound@hotmail.com>
2012-04-20 17:52:14 -0300

    Update src/Game/battle/BattleCharacter.h

--- src/Game/battle/BattleCharacter.h
+++ src/Game/battle/BattleCharacter.h
@@ -1,14 +1,56 @@
 #pragma once
 #include <character/BaseCharacter.h>
-#include <Entity.h>
+#include <Components/Component.h>
+#include <boost/signals2.hpp>
 namespace Game
 {
 	namespace Battle
 	{
-		class BattleCharacter : public Entity
+		class BattleCharacter;
+		typedef boost::signals2::signal<void (BattleCharacter* bchar, bool IsSim = false)> TimerOverflowSignal;
+		class BattleCharacter : public Component
 		{
 		public:
+			BattleCharacter(Character::BaseCharacter* owner) : Component(owner, owner != nullptr ? owner->GetName() + "BattleCharacterComponent" : "NoOwnerBattleCharacterComponent", "BattleCharacterComponent").
+				TimerValue(0), SimeTimerValue(0), OverflowRatio(0.f), CanTakeTurn(true), InSimulation(false), sm(owner != nullptr ? owner->GetComponentAs<Character::StatManager*>("StatManagerComponent") : nullptr)
+			{
+				
+			}
+			
+			void TickTimer(int ticks = 1)
+			{
+#ifdef DEBUG
+				if (sm != nullptr)
+				{
+#endif
+				if (InSimulation)
+				{
+					SimTimerValue += sm->GetBattleStat(Stat::Speed);
+				}
+				else
+				{
+					LiveTimerValue += sm->GetBattleStat(Stat::Speed);
+				}
+#ifdef DEBUG
+				}
+				else
+				{
+					std::cerr << "Stat Manager for battle character was null \n";
+				}
+#endif
+			}
+			
+			Character::BaseCharacter* GetWrapperCharacter() const
+			{
+				return WrappedCharacter;
+			}
 		private:
+			Character::StatsManager* sm;
+			int LiveTimerValue;
+			int SimTimerValue;
+			float OverflowRation;
+			bool CanTakeTurn;
+			bool InSimulation;
 			Character::BaseCharacter* WrappedCharacter;
 		};
 	}
